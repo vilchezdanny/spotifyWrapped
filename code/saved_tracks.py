1b8ee007f31a1126da41872bcf507045851ccf75
Daniel Vilchez <vilchez.daniel20@gmail.com>
2022-07-13 09:07:01 -0500

    code: fixed bug in saved_tracks.py

--- code/saved_tracks.py
+++ code/saved_tracks.py
@@ -1,6 +1,6 @@
-import time
 import spotipy
 import pandas as pd
+import numpy as np
 from spotipy.oauth2 import SpotifyOAuth
 
 LIBRARY_SCOPE = "user-library-read"
@@ -10,27 +10,38 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=LIBRARY_SCOPE))
 results = sp.current_user_saved_tracks(limit=50)
 tracks = results["items"]
 
-loop = 1
 while results["next"]:
-    print("Loop: {}".format(loop))
-    loop += 1
     results = sp.next(results)
     tracks.extend(results["items"])
-    # time.sleep(5)
 
 
-saved_tracks = []s
+saved_tracks = []
+
+loop = 1
 for track in tracks:
-    track_name = track["track"]["name"]
-    artist_name = track["track"]["artists"][0]["name"]
-    album_name = track["track"]["album"]["name"]
-    release_date = track["track"]["album"]["release_date"]
-    duration = track["track"]["duration_ms"]
-    song_popularity = track["track"]["popularity"]
-    album_img = track["track"]["album"]["images"][0]["url"]
-    track_uri = track["track"]["uri"]
-    artist_uri = track["track"]["artists"][0]["uri"]
-    album_uri = track["track"]["album"]["uri"]
+    loop += 1
+    if loop == 591:
+        track_name = track["track"]["name"]
+        artist_name = track["track"]["artists"][0]["name"]
+        album_name = track["track"]["album"]["name"]
+        release_date = track["track"]["album"]["release_date"]
+        duration = track["track"]["duration_ms"]
+        song_popularity = track["track"]["popularity"]
+        album_img = np.nan
+        track_uri = track["track"]["uri"]
+        artist_uri = track["track"]["artists"][0]["uri"]
+        album_uri = track["track"]["album"]["uri"]
+    else:
+        track_name = track["track"]["name"]
+        artist_name = track["track"]["artists"][0]["name"]
+        album_name = track["track"]["album"]["name"]
+        release_date = track["track"]["album"]["release_date"]
+        duration = track["track"]["duration_ms"]
+        song_popularity = track["track"]["popularity"]
+        album_img = track["track"]["album"]["images"][0]["url"]
+        track_uri = track["track"]["uri"]
+        artist_uri = track["track"]["artists"][0]["uri"]
+        album_uri = track["track"]["album"]["uri"]
 
     saved_tracks.append(
         [
